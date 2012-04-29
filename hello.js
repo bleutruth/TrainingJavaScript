1472d857fdda4cb2f693756c59cb620f19b724d1
s_aoyama <bleutruth@gmail.com>
2012-04-29 16:12:27 +0900

    ループを脱出するラベル構文を試してみた。

--- hello.js
+++ hello.js
@@ -1,4 +1,18 @@
 ﻿
+//	2重ループをラベル構文で脱出
+label_y:
+for(var y = 0; y < 20; y++){
+	for(var x = 0; x < 20; x++){
+		var xy = x * y;
+		if(xy > 150){
+			break label_y;
+		}
+		document.write(xy + ", ");
+	}
+	document.writeln("");
+}
+document.writeln("");
+
 //	数値を出力
 document.writeln(100);
 
