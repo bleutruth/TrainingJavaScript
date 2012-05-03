2875ab6a9440bb5b97c5cc846ed1150fa0909318
s_aoyama <bleutruth@gmail.com>
2012-05-04 03:59:51 +0900

    ソース整理。

--- hello.js
+++ hello.js
@@ -1,32 +1,46 @@
 ﻿
 //	例外
-try{
-	var i = xx;
-}
-catch(e){
-	document.writeln("Exception: " + e.message);
+var TestException = function()
+{
+	try{
+		var i = xx;
+	}
+	catch(e){
+		document.writeln('Exception: ' + e.message);
+	}
 }
 
 //	2重ループをラベル構文で脱出
-label_y:
-for(var y = 0; y < 20; y++){
-	for(var x = 0; x < 20; x++){
-		var xy = x * y;
-		if(xy > 150){
-			break label_y;
+var TestLoop = function()
+{
+	label_y:
+	for(var y = 0; y < 20; y++){
+		for(var x = 0; x < 20; x++){
+			var xy = x * y;
+			if(xy > 150){
+				break label_y;
+			}
+			document.write(xy + ', ');
 		}
-		document.write(xy + ", ");
+		document.writeln('');
 	}
-	document.writeln("");
+	document.writeln('');
 }
-document.writeln("");
 
-//	数値を出力
-document.writeln(100);
+//	出力
+var TestWrite = function()
+{
+	//	数値を出力
+	document.writeln(100);
 
-//	undefinedな変数を出力
-var undef;
-document.writeln(undef);
+	//	undefinedな変数を出力
+	var undef;
+	document.writeln(undef);
 
-//	文字列を出力
-document.writeln("HelloWorld!");
+	//	文字列を出力
+	document.writeln('HelloWorld!');
+}
+
+TestException();
+TestLoop();
+TestWrite();
